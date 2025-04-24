b074841ad0799a951820350677dd7f0ed31ce369
daureny <157032454+daureny@users.noreply.github.com>
2025-04-24 18:28:49 +0500

    vectorstore

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ def load_vectorstore():
 
     try:
         print("Попытка загрузки индекса из:", INDEX_PATH)
-        vectorstore = FAISS.load_local(INDEX_PATH, OpenAIEmbeddings())
+        vectorstore = FAISS.load_local(INDEX_PATH, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
         print("Векторное хранилище успешно загружено")
         return vectorstore
     except Exception as e:
